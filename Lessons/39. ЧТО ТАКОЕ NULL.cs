ca35f51b073d9921abce89a2ecaefc046cd4f173
sa1rex <c342@yandex.ru>
2022-02-13 14:15:01 +0200

    Add files via upload

---
+++ Lessons/39. ЧТО ТАКОЕ NULL.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons
+{
+
+    class MyClass
+    {
+
+    }
+
+    struct MyStruct
+    { 
+
+    }
+    internal class Program
+    {
+
+        // что такое null
+
+
+        static void Foo()
+        {
+
+            int[] a = new int[10];
+        
+        
+        
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            Foo();
+
+            /*
+            bool b;
+
+            int i;
+
+            double d;
+
+            Random random;
+
+            int[] arr;
+
+            MyClass myClass;
+
+            MyStruct myStruct;
+            
+             */
+
+
+
+
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
